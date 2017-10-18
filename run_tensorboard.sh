be596e32b931c90f94b1ef724f2413f24e5ba84c
Charles Reid <charlesreid1@gmail.com>
2017-10-18 05:39:45 -0700

    add banner to run tensorboard script

--- run_tensorboard.sh
+++ run_tensorboard.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo -e "\n\n"
+echo "**************************************"
+echo "***                                ***"
+echo "*** Open your browser to port 6006 ***"
+echo "***                                ***"
+echo "**************************************"
+echo -e "\n\n"
+
 /usr/local/bin/tensorboard --logdir=./logs
